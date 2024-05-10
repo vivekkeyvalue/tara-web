dd08b0930eb62ce738d51115f78c60ad05b4bbd9
vivekkeyvalue <vivek@keyvalue.systems>
2024-05-10 15:45:16 +0530

    fix: name changes

--- src/features/homework-management/pages/create-homework/CreateHomeWork.tsx
+++ src/features/homework-management/pages/create-homework/CreateHomeWork.tsx
@@ -1,3 +1,10 @@
-const CreateHomeWork = () => <div>Create Homework !</div>;
+import { Stepper } from '@components';
+import steps from '@features/homework-management/constants';
+
+const CreateHomeWork = () => (
+  <div>
+    <Stepper steps={steps} activeStep={0} />
+  </div>
+);
 
 export default CreateHomeWork;
